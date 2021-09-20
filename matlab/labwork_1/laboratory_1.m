ff96e74307feb01940b87e807391509863918519
alkosenk0 <alko7enko@gmail.com>
2021-09-20 16:11:01 +0300

    newton method

--- matlab/labwork_1/laboratory_1.m
+++ matlab/labwork_1/laboratory_1.m
@@ -4,13 +4,13 @@ clc;
 eps = 1e-12;
 step = 0.1;
 x = -pi:0.1:2*pi;
-titles = {'f(x) = 2x^4 - 8x^3 + 8x^2 - 1', 'f(x) = 2arctg(x) - x + 3', 'f(x) = 1 + e^{(-x)}'};
+titles = {'f(x) = 2x^4 - 8x^3 + 8x^2 - 1', 'f(x) = 2arctg(x) - x + 3'};
 colors = {'r', 'g', 'b', 'y'};
 
 count_func = size(titles);
 
 for j=1:count_func(2)
-    am_dicho = 0; am_hord = 0;
+    am_dicho = 0; am_hord = 0; am_newton = 0;
     
     subplot(count_func(2), 1, j);
     colors_size = size(colors);
@@ -38,20 +38,26 @@ for j=1:count_func(2)
                                            solutions(i, 2), eps, j);
         [root_hord, iter_hord] = hord(solutions(i, 1), ... 
                                       solutions(i, 2), eps, j);
+        [root_newt, iter_newt] = newton(solutions(i, 1), ... 
+                                      solutions(i, 2), eps, j);                          
         
         myfunc = @(x, j) f(x, j); % parameterized function
         fun = @(x) myfunc(x, j); % function of x alone
         fzero_ = fzero(fun,  [solutions(i, 1) solutions(i, 2)]);
+        % add roots
 
         plot(root_dicho, f(root_dicho, j), 'x');
         plot(root_hord, f(root_hord, j), '+');
+        plot(root_newt, f(root_newt, j), '*');
         plot(fzero_, f(fzero_, j), 's');
         
         am_dicho = am_dicho + iter_dich;
         am_hord = am_hord + iter_hord;
+        am_newton = am_newton + iter_newt;
         
-        if not(int16(root_hord) == int16(root_hord) && ...
-                                    int16(root_hord) == int16(fzero_))
+        if not(round(root_hord, 3) == round(root_hord, 3) && ...
+                            round(root_hord, 3) == round(fzero_, 3) && ...
+                                round(fzero_, 3) == round(root_newt, 3))
             testing = 0;
         end
     end
@@ -60,11 +66,26 @@ for j=1:count_func(2)
     fprintf('Step for tab was %.2f\n', step);
     fprintf('It takes %d iterations with hord method.\n', am_hord);
     fprintf('And %d iterations with dichotomy method.\n', am_dicho);
-    
+    fprintf('Newton method used %d iterations to solve problem,\n', am_newton);
+
     if am_dicho > am_hord
-        fprintf('Hord method made less for %d iterations.\n', am_dicho-am_hord);
-    elseif am_dicho < am_hord
-         fprintf('Dichotomy method made less for %d iterations.\n', am_hord-am_dicho);
+        if am_hord > am_newton    
+            fprintf('Newton method WON!\n');
+        else
+            fprintf('Hord method WON!\n');
+        end
+    elseif am_hord > am_dicho
+         if am_dicho > am_newton    
+            fprintf('Newton method WON!\n');
+        else
+            fprintf('Dichotomy method WON!\n');
+         end
+    else
+        if am_dicho > am_hord    
+            fprintf('Hord method WON!\n');
+        else
+            fprintf('Dichotomy method WON!\n');
+         end
     end
     
     if testing
@@ -76,9 +97,8 @@ for j=1:count_func(2)
 end
 
 function [y] = f(x, num)
-    F(1) = {@(x) 2*x.^4-8*x.^3+8*x.^2-1};
-    F(2) = {@(x) 2*atan(x) - x+3};
-    F(3) = {@(x) 1 - exp(1).^(x)};
+    F = {@(x) 2*x.^4-8*x.^3+8*x.^2-1;
+         @(x) 2*atan(x) - x+3};
     y = F{num}(x);
 end 
 
@@ -111,6 +131,29 @@ function [c, iterations] = hord(a, b, eps, num)
         end
         iterations = iterations + 1;
     end
+end
+
+function [xn, iterations] = newton(a, b, eps, num)
+    iterations = 0;
+    if f(a, num)*pr_func(a, num, 2) > 0
+        x = a;
+    else
+        x = b;
+    end
+    xn = x - (f(x, num)/pr_func(x, num, 1));
+    while abs(f(xn, num)) > eps
+        xn = x - (f(x, num)/pr_func(x, num, 1));
+        x = xn;
+        iterations = iterations + 1;
+    end
+end
+
+function [y] = pr_func(x, num, k)
+    F = {{@(x) 8*x.^3 - 24*x^2 + 16*x;
+          @(x) 24*x.^2 - 48*x + 16};
+         {@(x) 2/(1+x.^2) - 1;
+          @(x) (4*x)/((1-x.^2)^2)}};
+    y = F{num}{k}(x);
 end
 
 function [Array] = search_solutions(x, num)
