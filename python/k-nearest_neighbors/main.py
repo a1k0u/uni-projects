7cf00de98dfaf0096f69443bf653f6269c7c4601
alkosenk0 <alko7enko@gmail.com>
2021-10-29 20:51:29 +0300

    find nearest points

--- python/k-nearest_neighbors/main.py
+++ python/k-nearest_neighbors/main.py
@@ -1,6 +1,7 @@
 import pygame
 from random import randint
 import pygame.gfxdraw
+from math import sqrt
 
 pygame.init()
 
@@ -20,10 +21,24 @@ def draw_points(points: list, radius: int, screen):
 		pygame.gfxdraw.circle(screen, point[0][0], point[0][1], radius, point[1])
 
 
-def find_nearest(points: list) -> list:
+def find_nearest(points: list, mouse: tuple) -> list:
 	array = []
-	for j in points:
-		pass
+	for j in range(len(points)):
+		cd = points[j][0]
+		distance = sqrt((mouse[0] - cd[0])**2 + (mouse[1] - cd[1])**2)
+		array.append((distance, j))
+
+	array = sorted(array, key=lambda x: x[0])[:5]
+	return array[::]
+
+
+def draw_line(points: list, nearest_p: list, mouse: tuple, screen):
+	for i in range(len(nearest_p)):
+		print(nearest_p[i])
+		xy = points[nearest_p[i][1]][0]
+		pygame.gfxdraw.line(screen, mouse[0], mouse[1], xy[0], xy[1], (255, 255, 255))
+		pygame.gfxdraw.filled_circle(screen, mouse[0], mouse[1], 5, (255, 255, 255))
+		pygame.gfxdraw.filled_circle(screen, xy[0], xy[1], 3, (255, 255, 255))
 
 
 def main(width: int, height: int, fps: int):
@@ -32,17 +47,22 @@ def main(width: int, height: int, fps: int):
 	screen = pygame.display.set_mode((width, height), pygame.SHOWN)
 	pygame.display.set_caption("")
 	points = generate_points(width, height)
+	pygame.mouse.set_visible(False)
 	while application:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				application = False
 
+		mouse = pygame.mouse.get_pos()
+		p = find_nearest(points, mouse)
+
 		screen.fill((0, 0, 0))
 		draw_points(points, 10, screen)
+		draw_line(points, p, mouse, screen)
 		pygame.display.update()
 		clock.tick(fps)
 
 
 if __name__ == "__main__":
-	main(800, 600, 60)
+	main(1920, 1080, 60)
 	pygame.quit()
