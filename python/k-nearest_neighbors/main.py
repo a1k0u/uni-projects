b8136c35fdde53d35a6a1a023f8b768a65abd2e8
alkosenk0 <alko7enko@gmail.com>
2021-11-08 01:13:20 +0300

    add edit mode

--- python/k-nearest_neighbors/main.py
+++ python/k-nearest_neighbors/main.py
@@ -1,5 +1,6 @@
 from random import randint
 from math import sqrt, ceil
+from collections import Counter
 import pygame_widgets
 import pygame
 import pygame.gfxdraw
@@ -16,7 +17,11 @@ def get_pos():
 
 
 def change_color(color: list, func, number: int):
-    return tuple([el + number if func(el) else el for el in color])
+    return tuple(el + number if func(el) else el for el in color)
+
+
+def calculate_distance(start: tuple, end: tuple):
+    return sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
 
 
 class Application:
@@ -29,13 +34,15 @@ class Application:
         self.application = True
         self.points = []
         self.nearest_points = []
+        self.classify_points = []
         self.clock = pygame.time.Clock()
         self.radius = ceil(self.width * 0.005)
-        self.height_menu = ceil(self.height * 0.1)
+        self.menu_rect = pygame.Rect(0, 0, self.width, ceil(self.height * 0.1))
         self.classify_reset_flag = True
+        self.user_color_index = 0
 
         self.mouse = (0, 0)
-        self.parameters = {"points": self.width // 10, "colors": 2, "kNN": 3}
+        self.parameters = {"points": self.width // 10, "colors": 3, "kNN": 3}
         self.colors = colors.POINT_COLORS[: self.parameters["colors"]]
 
         self.screen = pygame.display.set_mode(
@@ -44,16 +51,14 @@ class Application:
 
         self.generate_points()
 
-        self.classify_points = []
-
         pygame.mouse.set_visible(False)
 
         sliders_type = {
             "points:": {
-                "min": 1,
+                "min": 2,
                 "max": self.parameters["points"],
-                "step": 10,
-                "initial": self.parameters["points"] // 2,
+                "step": 20,
+                "initial": self.parameters["points"] // 4,
             },
             "colors:": {
                 "min": 2,
@@ -61,7 +66,7 @@ class Application:
                 "step": 1,
                 "initial": 2,
             },
-            "kNN": {"min": 3, "max": 13, "step": 2, "initial": 3},
+            "kNN": {"min": 1, "max": 13, "step": 1, "initial": 3},
         }
 
         self.sliders_widget = [
@@ -109,10 +114,13 @@ class Application:
             handleOffColour=colors.SLIDER_HANDLER,
         )
 
+        self.button_widget = []
+
     def loop(self):
         while self.application:
             self.mouse = get_pos()
             self.set_parameters()
+
             if self.classify_reset_flag and self.toggle_widget.getValue():
                 self.classify_area()
                 self.classify_reset_flag = False
@@ -122,16 +130,20 @@ class Application:
                 self.draw_classify_area()
             self.draw_points()
             self.draw_menu_bar()
+            self.draw_color_buttons()
             self.output_info()
             self.handlers()
 
             self.nearest_points = self.find_nearest(*self.mouse)
-            if self.mouse[1] > self.height_menu and not self.toggle_widget.getValue():
+            if (
+                not self.menu_rect.collidepoint(self.mouse)
+                and not self.toggle_widget.getValue()
+            ):
                 self.draw_lines()
             self.draw_cursor()
 
-            pygame.display.update()
             self.clock.tick(self.fps)
+            pygame.display.update()
 
     def handlers(self):
         events = pygame.event.get()
@@ -139,24 +151,23 @@ class Application:
             if event.type == pygame.QUIT:
                 self.application = False
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if (
-                    event.button == 1
-                    and self.mouse[1] > self.height_menu
-                    and not self.toggle_widget.getValue()
-                ):
+                for i, button in enumerate(self.button_widget):
+                    if button.collidepoint(self.mouse):
+                        self.user_color_index = i
+                if event.button == 1 and not self.menu_rect.collidepoint(self.mouse):
                     self.points.append(
                         [
                             self.mouse[0],
                             self.mouse[1],
-                            self.find_common_color(self.nearest_points),
+                            self.colors[self.user_color_index],
                         ]
                     )
                     self.classify_reset_flag = True
         pygame_widgets.update(events)
 
-    def draw_gfx_circle(self, x: int, y: int, radius: int, color: tuple):
-        pygame.gfxdraw.aacircle(self.screen, x, y, radius, color)
-        pygame.gfxdraw.filled_circle(self.screen, x, y, radius, color)
+    def draw_gfx_circle(self, pos_x: int, pos_y: int, radius: int, color: tuple):
+        pygame.gfxdraw.aacircle(self.screen, pos_x, pos_y, radius, color)
+        pygame.gfxdraw.filled_circle(self.screen, pos_x, pos_y, radius, color)
 
     def draw_points(self):
         for point in self.points:
@@ -164,8 +175,7 @@ class Application:
                 self.draw_gfx_circle(
                     point[0], point[1], self.radius + 3, colors.OUTLINE
                 )
-                color = point[2]
-                color = change_color(list(color), lambda a: a, -92)
+                color = change_color(list(point[2]), lambda a: a, -92)
                 self.draw_gfx_circle(point[0], point[1], self.radius, color)
                 self.draw_gfx_circle(
                     point[0] - 1, point[1] - 1, self.radius - 1, point[2]
@@ -195,9 +205,7 @@ class Application:
         self.screen.fill(colors.BLACK)
 
     def draw_menu_bar(self):
-        pygame.gfxdraw.box(
-            self.screen, [0, 0, self.width, self.height_menu], colors.BLACK
-        )
+        pygame.gfxdraw.box(self.screen, self.menu_rect, colors.BLACK)
 
     def draw_classify_area(self):
         for rect in self.classify_points:
@@ -212,59 +220,76 @@ class Application:
                 rect[2],
             )
 
+    def draw_color_buttons(self):
+        for i, button in enumerate(self.button_widget):
+            x, y, width, height = button
+            if i == self.user_color_index:
+                self.draw_gfx_circle(
+                    x + width // 2, y + width // 2, width // 2 + 5, colors.WHITE
+                )
+            self.draw_gfx_circle(
+                x + width // 2, y + width // 2, width // 2, self.colors[i]
+            )
+
     def generate_points(self):
         self.points = [
             [
                 randint(self.radius, self.width - self.radius),
-                randint(self.height_menu, self.height - self.radius),
+                randint(self.menu_rect.height, self.height - self.radius),
                 self.colors[randint(0, len(self.colors) - 1)],
             ]
             for i in range(self.parameters["points"])
         ]
 
-    def find_nearest(self, x, y):
-        find_points = []
-        for i, point in enumerate(self.points):
-            distance = sqrt((x - point[0]) ** 2 + (y - point[1]) ** 2)
-            find_points.append((distance, i))
-
+    def find_nearest(self, pos_x: int, pos_y: int):
+        find_points = [
+            [calculate_distance(point, (pos_x, pos_y)), i]
+            for i, point in enumerate(self.points)
+        ]
         find_points.sort(key=lambda a: a[0])
+
         return find_points[: min(self.parameters["kNN"], len(find_points))]
 
-    def find_common_color(self, points):
-        points_color = {}
-        for point in points:
-            index = point[1]
-            color = self.points[index][2]
-            points_color.setdefault(color, 0)
-            points_color[color] += 1
-        return max(points_color, key=points_color.get)
+    def find_common_color(self, points: list):
+        points_color = Counter([self.points[index[1]][2] for index in points])
+        return points_color.most_common()[0][0]
 
     def classify_area(self):
-        self.classify_points.clear()
-        for y in range(0, self.height, self.radius + ceil(self.width * 0.0088)):
-            for x in range(0, self.width, self.radius + ceil(self.width * 0.0088)):
-                points = self.find_nearest(x, y)
-                color = self.find_common_color(points)
-                color = change_color(list(color), lambda a: not a, 92)
-                self.classify_points.append([x, y, color])
+        self.classify_points = [
+            [
+                x,
+                y,
+                change_color(
+                    self.find_common_color(self.find_nearest(x, y)), lambda a: not a, 92
+                ),
+            ]
+            for y in range(0, self.height, self.radius + ceil(self.width * 0.0088))
+            for x in range(0, self.width, self.radius + ceil(self.width * 0.0088))
+        ]
 
     def set_parameters(self):
-        if any(
-            self.parameters[key] != self.sliders_widget[i].getValue()
-            for i, key in enumerate(self.parameters)
-        ):
-            for i, key in enumerate(self.parameters):
+        for i, key in enumerate(self.parameters):
+            if self.parameters[key] != self.sliders_widget[i].getValue():
+                self.classify_reset_flag = True
                 self.parameters[key] = self.sliders_widget[i].getValue()
-            self.colors = colors.POINT_COLORS[: self.parameters["colors"]]
-            self.generate_points()
-            self.classify_area()
+                self.colors = colors.POINT_COLORS[: self.parameters["colors"]]
+                self.generate_points()
+
+                self.button_widget = [
+                    pygame.Rect(
+                        ceil(self.width * 0.715 + self.width * i * 0.035),
+                        self.height * 0.03,
+                        self.radius * 4,
+                        self.radius * 4,
+                    )
+                    for i in range(len(self.colors))
+                ]
 
     def output_info(self):
         for i, key in enumerate(self.parameters):
             self.text_widget[i].setText(f"Amount of {key}: {self.parameters[key]}")
         self.text_widget[-1].setText(
-            "Classify mode" if self.toggle_widget.getValue() else "Edit mode"
+            "Classify mode" if self.toggle_widget.getValue() else "Spider mode"
         )
 
 
