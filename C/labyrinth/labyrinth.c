090b98929d3d95c0d6cb04c6dc0806abc7d080cc
alkosenk0 <alko7enko@gmail.com>
2021-11-21 12:38:47 +0300

    add positions, handlers and move

--- C/labyrinth/labyrinth.c
+++ C/labyrinth/labyrinth.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 
 char** initMap(char** map, int SIZE) {
     map = (char**)calloc(SIZE, sizeof(char*));
@@ -15,7 +16,7 @@ void fillMap(char** map, int size) {
                     (y == size-1) || (x == size-1))
                 map[x][y] = '#';
             else
-                map[x][y] = '*';
+                map[x][y] = ' ';
 
 }
 
@@ -27,10 +28,60 @@ void printMap(char** map, int size) {
     }
 }
 
+void printInfo() {
+    printf("\nUse SHIFT + < - to correct START position(@).\n");
+    printf("Use SHIFT + > - to correct END position(&).\n");
+    printf("\nUse WASD to move.");
+    printf("\n\n");
+}
+
 int main() {
-    int size = 20;
+    int size = 10;
+
     char** map = initMap(map, size);
-    fillMap(map, size);
-    printMap(map, size);
+    char button;
+
+    int start_x = 1, start_y = 1;
+    int end_x = size - 2, end_y = size - 2;
+
+    int x = start_x, y = start_y;
+    int choosen = 0;
+
+    do {
+        fillMap(map, size);
+        map[start_x][start_y] = '@';
+        map[end_x][end_y] = '&';
+
+        printMap(map, size);
+        printInfo();
+
+        button = getch();
+        if (button == 'w') y--;
+        if (button == 's') y++;
+        if (button == 'a') x--;
+        if (button == 'd') x++;
+
+        if (button == '>') {
+            x = end_x;
+            y = end_y;
+            choosen = 1;
+        } else if (button == '<') {
+            x = start_x;
+            y = start_y;
+            choosen = 0;
+        }
+
+        if (!choosen) {
+            start_x = x;
+            start_y = y;
+        } else if (choosen) {
+            end_x = x;
+            end_y = y;
+        }
+
+
+
+        system("cls");
+    } while (button != 'e');
     return 0;
 }
